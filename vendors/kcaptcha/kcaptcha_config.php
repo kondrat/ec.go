69821b18f4b4142d3ebdd30a34032ce415fd578c
unknown <akv@.air.local>
2010-11-09 21:46:56 +0300

    more
    Signed-off-by: unknown <akv@.air.local>

--- vendors/kcaptcha/kcaptcha_config.php
+++ vendors/kcaptcha/kcaptcha_config.php
@@ -5,8 +5,8 @@
 $alphabet = "0123456789abcdefghijklmnopqrstuvwxyz"; # do not change without changing font files!
 
 # symbols used to draw CAPTCHA
-//$allowed_symbols = "0123456789"; #digits
-$allowed_symbols = "23456789abcdeghkmnpqsuvxyz"; #alphabet without similar symbols (o=0, 1=l, i=j, t=f)
+$allowed_symbols = "0123456789"; #digits
+//$allowed_symbols = "23456789abcdeghkmnpqsuvxyz"; #alphabet without similar symbols (o=0, 1=l, i=j, t=f)
 
 # folder with fonts
 $fontsdir = 'fonts';	
@@ -39,5 +39,5 @@ $foreground_color = array(mt_rand(0,100), mt_rand(0,100), mt_rand(0,100));
 $background_color = array(mt_rand(200,255), mt_rand(200,255), mt_rand(200,255));
 
 # JPEG quality of CAPTCHA image (bigger is better quality, but larger file size)
-$jpeg_quality = 90;
+$jpeg_quality = 70;
 ?>
